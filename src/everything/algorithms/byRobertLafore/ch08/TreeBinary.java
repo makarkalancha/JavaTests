2cd0fa786a2fd79d726013c6b2cbc1e65a48bf9d
makarkalancha <makarkalancha@gmail.com>
2015-01-12 17:26:45 -0500

    algorithm: tree, print

--- src/everything/algorithms/byRobertLafore/ch08/TreeBinary.java
+++ src/everything/algorithms/byRobertLafore/ch08/TreeBinary.java
@@ -12,20 +12,83 @@ public class TreeBinary<T extends Comparable> extends AbstractTree<T> {
 
     @Override
     public void delete(T key) {
-        System.out.println("NOT implemented yet!");
+        boolean isLeftNode = false;
+        NodeCh08<T> current = _root;
+        NodeCh08<T> previous = _root;
+        while (current != null && key.compareTo(current.iData) != 0) {
+            previous = current;
+            if (key.compareTo(current.iData) == -1) {
+                current = current.leftChild;
+                isLeftNode = true;
+            } else if (key.compareTo(current.iData) == 1) {
+                current = current.rightChild;
+                isLeftNode = false;
+            }
+
+//            if(key.compareTo(current.iData) == 0){
+//                return null;
+//            }
+        }
+
+        if (current.leftChild == null && current.rightChild == null) {
+//            if(current.iData.compareTo(previous.iData) == -1){
+            if(current == _root) {
+                _root = null;
+            }else if(isLeftNode){
+                previous.leftChild = null;
+            } else {
+                previous.rightChild = null;
+            }
+        }else if (current.leftChild == null || current.rightChild == null) {
+            if(current == _root) {
+                _root = (current.leftChild == null) ? current.rightChild : current.leftChild;
+            }else if(isLeftNode){
+                previous.leftChild = (current.leftChild == null) ? current.rightChild : current.leftChild;
+            } else {
+                previous.rightChild = (current.leftChild == null) ? current.rightChild : current.leftChild;
+            }
+        } else {
+            NodeCh08<T> successor = getSuccessor(current);
+            if(current == _root){
+                _root = successor;
+            } else if (isLeftNode) {
+                previous.leftChild = successor;
+            } else {
+                previous.rightChild = successor;
+                successor.leftChild = current.leftChild;
+            }
+        }
+    }
+
+    private NodeCh08<T> getSuccessor(NodeCh08<T> delNode) {
+        NodeCh08<T> successorParent = delNode;
+        NodeCh08<T> successor = delNode;
+        NodeCh08<T> current = delNode.rightChild;
+        while(current != null) {
+            successorParent = successor;
+            successor = current;
+            current = current.leftChild;
+        }
+
+        if(successor != delNode.rightChild) {
+            successorParent.leftChild = successor.rightChild;
+            successor.rightChild = delNode.rightChild;
+        }
+
+        return successor;
     }
 
     @Override
     public NodeCh08<T> find(T key) {
         NodeCh08<T> current = _root;
-        while (current.iData.equals(key)) {
+        while (current != null && current.iData.compareTo(key) != 0) {
             if (key.compareTo(current.iData) == -1) {
-                current = _root.leftChild;
+                current = current.leftChild;
             } else if (key.compareTo(current.iData) == 1) {
-                current = _root.rightChild;
+                current = current.rightChild;
             }
 
-            if(current == null){
+            if (current == null) {
                 return null;
             }
 
@@ -77,5 +140,29 @@ public class TreeBinary<T extends Comparable> extends AbstractTree<T> {
             System.out.print(current.iData + " ; ");
             inOrderTraversalBook(current.rightChild);
         }
+    }
+
+    private T getMinimum(NodeCh08<T> root) {
+        NodeCh08<T> current = root;
+        NodeCh08<T> previous = root;
+        while(current != null) {
+            previous = current;
+            current = current.leftChild;
+        }
+        return previous.iData;
+    }
+
+    public T getMinimum() {
+        return getMinimum(_root);
+    }
+
+    public T getMaximum() {
+        NodeCh08<T> current = _root;
+        NodeCh08<T> previous = _root;
+        while(current != null) {
+            previous = current;
+            current = current.rightChild;
+        }
+        return previous.iData;
     }
 }
--- src/everything/algorithms/byRobertLafore/ch08/TreeDemo.java
+++ src/everything/algorithms/byRobertLafore/ch08/TreeDemo.java
@@ -17,10 +17,32 @@ public class TreeDemo {
         integerTreeBinary.insert(new Integer(40));
         integerTreeBinary.insert(new Integer(100));
         integerTreeBinary.insert(new Integer(80));
+        integerTreeBinary.insert(new Integer(110));
 
-//        integerTreeBinary.displayTree();
+//        System.out.println("find: " + integerTreeBinary.find(100));
+//
+////        integerTreeBinary.displayTree();
+//
+//        integerTreeBinary.inOrderTraversal();
+//
+//        System.out.println();
+//        System.out.println("min: "+integerTreeBinary.getMinimum());
+//        System.out.println("max: "+integerTreeBinary.getMaximum());
+//
+////        System.out.println("find: " + integerTreeBinary.find(30));
+////        integerTreeBinary.delete(40);
+////        System.out.println("find: " + integerTreeBinary.find(30));
+//
+//
+//        System.out.println("find: " + integerTreeBinary.find(50));
+//        integerTreeBinary.delete(30);
+//        System.out.println("find: " + integerTreeBinary.find(50));
+//
+////        integerTreeBinary.displayTree();
+//        integerTreeBinary.inOrderTraversal();
 
-        integerTreeBinary.inOrderTraversal();
+
+        BTreePrinter.printNode(twenty);
 
     }
 }
