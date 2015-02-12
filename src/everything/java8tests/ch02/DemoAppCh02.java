f19b3bee76d89952247b283df8614b22e8ad4e9e
makarkalancha <makarkalancha@gmail.com>
2015-02-12 11:13:51 -0500

    java8: streams

--- src/everything/java8tests/ch02/DemoAppCh02.java
+++ src/everything/java8tests/ch02/DemoAppCh02.java
@@ -44,9 +44,89 @@ public class DemoAppCh02 {
 //        System.out.println("func#008: lower case streams");
 //        DemoAppCh02.func008();
 
-        System.out.println("func#009: lower case streams");
-        DemoAppCh02.func009();
+//        System.out.println("func#009: first character streams");
+//        DemoAppCh02.func009();
+
+//        System.out.println("func#010: character streams");
+//        DemoAppCh02.func010();
+
+//        System.out.println("func#011: infinite streams with limit");
+//        DemoAppCh02.func011();
+
+//        System.out.println("func#012: skip 2 streams");
+//        DemoAppCh02.func012();
+
+//        System.out.println("func#013: concat streams");
+//        DemoAppCh02.func013();
+
+//        System.out.println("func#014: peek streams");
+//        DemoAppCh02.func014();
+
+        System.out.println("func#015: distinct");
+        DemoAppCh02.func015();
     }
+
+    public static void func015() {
+        Stream<String> uniqueWords = Stream.of("merrily", "merrily", "merrily", "gently").distinct();
+        uniqueWords.forEach(System.out::println);
+    }
+
+    public static void func014() {
+        Object[] powers = Stream.iterate(1.0, p -> p * 2)
+                .peek(e -> System.out.println("Fetching " + e))
+                .limit(20)
+                .toArray();
+        System.out.println(Arrays.toString(powers));
+    }
+
+    public static void func013() {
+        Stream<Character> characterStream = Stream.concat(characterStream("hello"),characterStream("world"));
+        characterStream.forEach(System.out::println);
+    }
+
+    public static void func012() {
+        List<String> wordList = new ArrayList<>();
+        wordList.add("ADD");
+        wordList.add("me");
+        wordList.add("TO");
+        wordList.add("your");
+        wordList.add("LIST");
+
+        Stream<String> stream = wordList.stream().skip(2);
+        stream.forEach(System.out::println);
+    }
+
+    public static void func011() {
+        System.out.println("random from 7 to 22");
+        Stream<Integer> stream = Stream.generate(() -> (int)(Math.random() * 20 + 7)).limit(10);
+        stream.forEach(System.out::println);
+    }
+
+    public static void func010() {
+        List<String> wordList = new ArrayList<>();
+        wordList.add("ADD");
+        wordList.add("me");
+        wordList.add("TO");
+        wordList.add("your");
+        wordList.add("LIST");
+
+        System.out.println("stream of stream:");
+        Stream<Stream<Character>> characterStream = wordList.stream().map(w -> characterStream(w));
+        characterStream.forEach(stream -> stream.forEach(System.out::println));
+
+        System.out.println("\nflat map:");
+        Stream<Character> characterStream1 = wordList.stream().flatMap(w -> characterStream(w));
+        characterStream1.forEach(System.out::println);
+    }
+
+    public static Stream<Character> characterStream(String s) {
+        List<Character> result = new ArrayList<>();
+        for(char c : s.toCharArray()) {
+            result.add(c);
+        }
+        return result.stream();
+    }
+
 
     public static void func009() {
         List<String> wordList = new ArrayList<>();
@@ -58,7 +138,7 @@ public class DemoAppCh02 {
 
         Stream<String> wordStream = wordList.stream();
         Stream<Character> charAtWordStream = wordStream.map(s -> s.charAt(0));
-        charAtWordStream .forEach(System.out::println);
+        charAtWordStream.forEach(System.out::println);
     }
 
     public static void func008() {
