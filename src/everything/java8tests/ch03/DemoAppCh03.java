fb0104398bb284037bf93502a40c3d84e415c3dd
Makar Kalancha <makarkalancha@gmail.com>
2015-02-21 21:07:25 -0500

    java8: lambda ch03

--- src/everything/java8tests/ch03/DemoAppCh03.java
+++ src/everything/java8tests/ch03/DemoAppCh03.java
@@ -1,6 +1,7 @@
 package everything.java8tests.ch03;
 
 import javax.sound.midi.Soundbank;
+import java.util.function.IntConsumer;
 import java.util.function.Supplier;
 
 /**
@@ -10,8 +11,27 @@ import java.util.function.Supplier;
  */
 public class DemoAppCh03 {
     public static void main(String[] args) {
-        System.out.println("func001: supplier");
-        func001();
+//        System.out.println("func001: supplier");
+//        func001();
+
+        System.out.println("func002: choosing functional interface");
+        func002();
+    }
+
+    public static void func002(){
+        repeat1(3, (i)->System.out.println("-->"+i));
+    }
+
+    public static void repeat2(int n, Runnable runnable) {
+        for (int i = 0; i < n; i++) {
+            runnable.run();
+        }
+    }
+
+    public static void repeat1(int n, IntConsumer action) {
+        for (int i = 0; i < n; i++) {
+            action.accept(i);
+        }
     }
 
     public static void func001(){
