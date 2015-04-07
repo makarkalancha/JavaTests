16d482c043cff025e5a3ccebc77bb6a4a29e14e6
makarkalancha <makarkalancha@gmail.com>
2015-04-07 09:41:00 -0400

    number tests

---
+++ src/everything/numbertest/DoubleTests.java
@@ -0,0 +1,13 @@
+package everything.numbertest;
+
+/**
+ * Created by mcalancea on 2015-04-07.
+ */
+public class DoubleTests {
+    public static void main(String[] args) {
+        double numberENotation = 1e-6;
+        double number = 0.000001;
+        System.out.println(numberENotation);
+        System.out.println(number);
+    }
+}
