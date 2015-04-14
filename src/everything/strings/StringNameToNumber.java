7966b4108ce4953811d8aac85f17bfb0bda0a07b
makarkalancha <makarkalancha@gmail.com>
2015-04-14 08:57:58 -0400

    string name number

--- src/everything/strings/StringNameToNumber.java
+++ src/everything/strings/StringNameToNumber.java
@@ -10,7 +10,9 @@ public class StringNameToNumber {
      */
     public static void main(String[] args) {
 //        String[] stringArray = {"A", "a", "b", "c", "d"};
-        String[] stringArray = {"Baidu","Clone-Sync Emails", "Bulk Import", "Bugs", "Invalid atrkid", "My Jiras", "!Favorite", "Team 6", "JBoss", "Jenkins", "Log", "Management", "Oracle", "Oracle DB", "Setup", "Ticket"};
+        String[] stringArray = {"Baidu","Clone-Sync Emails", "Bulk Import", "Bugs", "Invalid atrkid", "My Jiras",
+                "!Favorite", "Team 6", "JBoss", "Jenkins", "Log", "Management", "Oracle", "Oracle DB", "Setup",
+                "Ticket","Java Guilde","Personal"};
         for (String st : stringArray) {
             System.out.println(st + ":" + getNumber(st));
         }
