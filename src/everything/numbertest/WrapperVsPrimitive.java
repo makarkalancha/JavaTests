f3a5544e85ae3c5259fd231ebfb6dee9e497c772
makarkalancha <makarkalancha@gmail.com>
2015-11-24 14:48:33 -0500

    wrapper vs primitive

--- src/everything/numbertest/WrapperVsPrimitive.java
+++ src/everything/numbertest/WrapperVsPrimitive.java
@@ -9,8 +9,13 @@ public class WrapperVsPrimitive {
         Long long1 = null;
         long long2 = 100L;
         Long long3 = new Long(100);
-        System.out.println("null == 100L:"+(long1 == long2));//NPE
+//        System.out.println("null == 100L:"+(long1 == long2));//NPE
 //        System.out.println("null == new Long(100L):"+(long1 == long3));
+
+//        System.out.println(long1.toString());//NPE
+
+        Double thousand23 = 23_000.0;
+        System.out.println(thousand23 / 1_000);
     }
 
 }
