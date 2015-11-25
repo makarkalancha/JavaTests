3b18bdd901359e838ff657b1203562cf579a819c
makarkalancha <makarkalancha@gmail.com>
2015-11-25 15:02:08 -0500

    java-funcation package

---
+++ src/everything/java8tests/java_util_function/ConsumerTest.java
@@ -0,0 +1,21 @@
+package everything.java8tests.java_util_function;
+
+import java.util.function.Consumer;
+
+/**
+ * Created by mcalancea on 2015-11-24.
+ */
+public class ConsumerTest {
+    //https://blog.idrsolutions.com/2015/03/java-8-consumer-supplier-explained-in-5-minutes/
+    public static void main(String[] args) {
+        Consumer<String> consumer = ConsumerTest::printNames;
+        consumer.accept("Jeremy");
+        consumer.accept("Paul");
+        consumer.accept("Richard");
+    }
+
+    public static void printNames(String name){
+        System.out.println(name);
+    }
+
+}
---
+++ src/everything/java8tests/java_util_function/SupplierTest.java
@@ -0,0 +1,29 @@
+package everything.java8tests.java_util_function;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
+/**
+ * Created by mcalancea on 2015-11-24.
+ */
+public class SupplierTest {
+    //https://blog.idrsolutions.com/2015/03/java-8-consumer-supplier-explained-in-5-minutes/
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("Jeremy");
+        names.add("Paul");
+        names.add("Richard");
+
+        names.stream()
+                .forEach(name -> {
+                    printNames(()-> name);
+                });
+    }
+
+    public static void printNames(Supplier<String> arg){
+        System.out.println(arg.get());
+    }
+
+}
+
