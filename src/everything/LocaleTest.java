9f7f1d0daaf3a1116e11805503b8bc963aad34de
makarkalancha <makarkalancha@gmail.com>
2016-02-12 09:17:39 -0500

    locale

--- src/everything/LocaleTest.java
+++ src/everything/LocaleTest.java
@@ -11,8 +11,12 @@ public class LocaleTest {
     public static void main(String[] args) {
         Locale defaultLocale = Locale.getDefault();
         Locale en = new Locale("en","US");
+        Locale ja = new Locale("ja","JA");
         Locale.setDefault(en);
         System.out.println(defaultLocale.getDisplayName());
         System.out.println(en.getDisplayName());
+        System.out.println(en.getISO3Language());
+        System.out.println(en.getLanguage());
+        System.out.println(ja.getLanguage());
     }
 }
