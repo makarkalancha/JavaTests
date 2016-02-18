b0e3b3e305dc722b8d65b18008b95b8e989b5893
Makar Kalancha <makarkalancha@gmail.com>
2016-02-17 22:26:11 -0500

    h2 tests

--- src/everything/JDBC/h2_tests/H2DbConstants.java
+++ src/everything/JDBC/h2_tests/H2DbConstants.java
@@ -6,13 +6,13 @@ package everything.JDBC.h2_tests;
 public class H2DbConstants {
     public static final String DB_DRIVER = "org.h2.Driver";
     public static final String DB_DIR = "~";
+    public static final String DB_SCHEMA = "FINANCE";
     public static final String DB_NAME = "test";
-    public static final String DB_CONNECTION = "jdbc:h2:"+DB_DIR+"/"+DB_NAME+";IFEXISTS=TRUE";
+    public static final String DB_CONNECTION = "jdbc:h2:"+DB_DIR+"/"+DB_NAME+";IFEXISTS=TRUE;SCHEMA="+DB_SCHEMA;
     public static final String DB_DIR1 = "~/smart_finance";
     public static final String DB_NAME1 = "finance";
     public static final String DB_CONNECTION1 = "jdbc:h2:"+DB_DIR1+"/"+DB_NAME1;
-    public static final String DB_CONNECTION1_IFEXISTS = "jdbc:h2:"+DB_DIR1+"/"+DB_NAME1+";IFEXISTS=TRUE";
+    public static final String DB_CONNECTION1_IFEXISTS = "jdbc:h2:"+DB_DIR1+"/"+DB_NAME1+";IFEXISTS=TRUE;SCHEMA="+DB_SCHEMA;
     public static final String DB_USER = "root";
     public static final String DB_PASSWORD = "root";
-
 }
