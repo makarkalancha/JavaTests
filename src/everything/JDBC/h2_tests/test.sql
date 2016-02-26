1087368c37379b03592c573bce962bc67fb9d470
makarkalancha@gmail.com <makarkalancha@gmail.com>
2016-02-26 13:44:40 -0500

    h2: select

--- src/everything/JDBC/h2_tests/test.sql
+++ src/everything/JDBC/h2_tests/test.sql
@@ -2,10 +2,10 @@ SET SCHEMA FINANCE;
 CREATE ALIAS CREATE_DATE_UNIT_TABLE FOR "com.everything.JDBC.h2.Functions.createDateUnitTable";
 CREATE ALIAS INSERT_SELECT_DATE FOR "com.everything.JDBC.h2.Functions.insertSelectDate";
 
+CALL CREATE_DATE_UNIT_TABLE();
 --set @date = call PARSEDATETIME("2016-02-12","yyyy-MM-dd");
 --set @date = "2016-02-12";
 --CALL INSERT_SELECT_DATE(@date);
 select @date1 := PARSEDATETIME('2016-02-12','yyyy-MM-dd');
 CALL INSERT_SELECT_DATE(@date1)
 
-CALL CREATE_DATE_UNIT_TABLE();
