856c71729af9de9587de8f0575fe701efb4974fb
Makar Kalancha <makarkalancha@gmail.com>
2016-04-10 00:02:04 -0400

    memento

---
+++ src/everything/design_patterns/tests/memento/CakeTaker.java
@@ -0,0 +1,21 @@
+package everything.design_patterns.tests.memento;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * User: Makar Kalancha
+ * Date: 09/04/2016
+ * Time: 23:28
+ */
+public class CakeTaker {
+    private List<Memento> savedStates = new ArrayList<>();
+
+    public void saveState(Memento m){
+        savedStates.add(m);
+    }
+
+    public Memento getState(int index){
+        return savedStates.get(index);
+    }
+}
