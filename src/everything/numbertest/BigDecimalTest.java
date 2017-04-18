a5886487b70762e0f918e433b81caa85b31b5bbf
makarkalancha <makarkalancha@gmail.com>
2017-04-18 10:42:07 -0400

    big decimal test

---
+++ src/everything/numbertest/BigDecimalTest.java
@@ -0,0 +1,17 @@
+package everything.numbertest;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by Makar Kalancha
+ * Date: 18 Apr 2017
+ * Time: 08:50
+ */
+public class BigDecimalTest {
+    public static void main(String[] args) {
+        BigDecimal one = null;
+        BigDecimal two = new BigDecimal("2");
+        System.out.println(one.compareTo(two));
+
+    }
+}
