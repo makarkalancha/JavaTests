719a25ce17d8f5f0107e9e467fdde6fffadb14b8
makarkalancha <makarkalancha@gmail.com>
2017-06-13 08:52:45 -0400

    boolean test

--- src/everything/booleantest/BooleanMain.java
+++ src/everything/booleantest/BooleanMain.java
@@ -10,5 +10,10 @@ public class BooleanMain {
         Boolean third = null;
         boolean result1 = first || second && third;
         System.out.println(result1);
+
+//        boolean test1;
+//        System.out.println(test1);
+//        test1 = false;
+//        System.out.println(test1);
     }
 }
