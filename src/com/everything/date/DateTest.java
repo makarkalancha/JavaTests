b35ffe5657f68f06d7980aa5fdf39ab4320c89af
makarkalancha <makarkalancha@gmail.com>
2017-10-13 16:38:32 -0400

    date test

---
+++ src/com/everything/date/DateTest.java
@@ -0,0 +1,37 @@
+package com.everything.date;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+/**
+ * Created by Makar Kalancha
+ * Date: 11 Oct 2017
+ * Time: 16:59
+ */
+public class DateTest {
+    public static void main(String[] args) {
+        int retryCount = 3;
+        int addition = (retryCount + 1)* (retryCount + 1);
+        System.out.println(addition);
+
+        Date date = new Date();
+        System.out.println(date);
+        long millis = date.getTime();
+//        long minutes = TimeUnit.MILLISECONDS.toSeconds(millis);
+        long minutes = millis;
+
+
+        System.out.println(minutes);
+//        Date newDate = new Date(TimeUnit.SECONDS.toMillis(minutes));
+        Date newDate = new Date(minutes);
+        System.out.println(newDate);
+
+        System.out.println("========================================");
+        addition = new Double(Math.pow(retryCount + 1, 2)).intValue();
+        LocalDateTime localDateTime = LocalDateTime.now();
+        LocalDateTime newLocalDateTime = localDateTime.plusMinutes(addition);
+        System.out.println(addition);
+        System.out.println(localDateTime);
+        System.out.println(newLocalDateTime);
+    }
+}
