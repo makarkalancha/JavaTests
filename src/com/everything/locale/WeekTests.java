a9060baa69c751ea4a88bed806f8ec72313fffe4
Makar Kalancha <makarkalancha@gmail.com>
2017-12-11 19:40:15 -0500

    week day: done

--- src/com/everything/locale/WeekTests.java
+++ src/com/everything/locale/WeekTests.java
@@ -5,6 +5,7 @@ import java.time.Month;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -20,12 +21,15 @@ public class WeekTests {
         int second = 3;
         int third = 6;
         List<Integer> weekDays = Arrays.asList(first, second, third);
-        LocalDate start = LocalDate.of(2017, Month.DECEMBER, 5);
+        Collections.sort(weekDays);
+        LocalDate start = LocalDate.of(2017, Month.DECEMBER, 12);
         int maxOccurrence = 7;
+        int repeatEvery = 2;
 
         /////////////////////method
         List<LocalDate> result = new ArrayList<>(Arrays.asList(start));
         final int WEEK = 7;
+        int cycle = WEEK * repeatEvery;
 
         List<Integer> cycleWeekDays = new ArrayList<>();
         cycleWeekDays.addAll(weekDays);
@@ -35,47 +39,49 @@ public class WeekTests {
         System.out.println(currentDate.getDayOfWeek().getValue());
         //get closest index to start date week day
         Map<Integer, Integer> differenceToWeekDaysIndex = new HashMap<>();
+//        BiMap<Integer, Integer> differenceToWeekDaysIndex = HashBiMap.create();
         for (int number = 0; number < weekDays.size(); number++) {
             int difference = (weekDays.get(number) - currentDate.getDayOfWeek().getValue() < 0) ?
-                    WEEK + weekDays.get(number) - currentDate.getDayOfWeek().getValue() :
+                    cycle + weekDays.get(number) - currentDate.getDayOfWeek().getValue() :
                     weekDays.get(number) - currentDate.getDayOfWeek().getValue();
 
             differenceToWeekDaysIndex.put(difference, number);
         }
         System.out.println("differenceToIndex:" + differenceToWeekDaysIndex);
-        int i = differenceToWeekDaysIndex.entrySet().stream()
-                .filter(entry -> entry.getKey() > 0)
+        Map.Entry<Integer, Integer> minEntry = differenceToWeekDaysIndex.entrySet().stream()
                 .min((entry1, entry2) -> entry1.getKey().compareTo(entry2.getKey()))
-                .map(entry -> entry.getValue())
-                .orElse(-1);
-        System.out.println("i: " + i);
-        System.out.println("next day: " + weekDays.get(i));
+                .orElse(null);
+        System.out.println(String.format("minEntry: %s, %s", minEntry.getKey(), minEntry.getValue()));
+        System.out.println("next day: " + weekDays.get(minEntry.getValue()));
+
+        currentDate = currentDate.plus(minEntry.getKey(), ChronoUnit.DAYS);
+        result.add(currentDate);
 
         List<Integer> daysToAdd = new ArrayList<>();
         int current = cycleWeekDays.get(0);
-        for (; i < cycleWeekDays.size(); i++) {
+
+        for (int i = minEntry.getValue(); i < cycleWeekDays.size(); i++) {
             if(cycleWeekDays.get(i) > current) {
                 daysToAdd.add(cycleWeekDays.get(i) - current);
             } else if (cycleWeekDays.get(i) < current){
-                daysToAdd.add(cycleWeekDays.get(i) + WEEK - current);
+                daysToAdd.add(cycleWeekDays.get(i) + cycle - current);
             }
             current = cycleWeekDays.get(i);
         }
 
-        start:2
-        diff:1
-        -(+1)->3-(+3)->6-(+2)->1-(+2)->3-(+3)->6....
+//        start:2
+//        diff:1
+//        -(+1)->3-(+3)->6-(+2)->1-(+2)->3-(+3)->6....
 
-        System.out.println("daysToAdd: " + daysToAdd);
 
-        for (; i < daysToAdd.size() && result.size() < maxOccurrence; i++) {
-//            currentDate.getDayOfWeek().getValue()
+        for (int i = minEntry.getValue(); result.size() < maxOccurrence; i++) {
+            if(i >= daysToAdd.size()){
+                i = 0;
+            }
             currentDate = currentDate.plus(daysToAdd.get(i), ChronoUnit.DAYS);
             result.add(currentDate);
         }
 
-        System.out.println(daysToAdd);
         System.out.println(result);
-
     }
 }
