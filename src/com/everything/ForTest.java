0b55114c9abc791b72dfa7e13774a21e850c3448
makarkalancha <makarkalancha@gmail.com>
2018-04-11 16:01:50 -0400

    loop tests

--- src/com/everything/ForTest.java
+++ src/com/everything/ForTest.java
@@ -7,7 +7,7 @@ package com.everything;
  */
 public class ForTest {
     public static void main(String[] args) {
-        for(int i=0;i<0;i++){
+        for (int i = 0; i < 0; i++) {
             System.out.println(i);
         }
 
@@ -15,5 +15,13 @@ public class ForTest {
         System.out.println("b)1 + 2 + 3 + 4.0="+(1 + 2 + 3 + 4.0));
         System.out.println("c)4.1 >= 4="+(4.1 >= 4));
         System.out.println("d)1 + 2 + \"3\"="+(1 + 2 + "3"));
+
+
+        int count = -1;
+        do {
+            System.out.println("in the loop count = " + count);
+            count--;
+        } while (count >= 0);
+        System.out.println("outside of the loop count = " + count);
     }
 }
