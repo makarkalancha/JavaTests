0e3746e98f8c599b41c1777b4beabbee549cfc12
makarkalancha <makarkalancha@gmail.com>
2018-06-15 17:10:03 -0400

    recursive demo in progress

---
+++ src/com/everything/parsers/RecursiveDemo.java
@@ -0,0 +1,37 @@
+package com.everything.parsers;
+
+/**
+ * Created by mcalancea
+ * Date: 15 Jun 2018
+ * Time: 13:08
+ */
+public class RecursiveDemo {
+    private String[] dimensionNames = {"level", "networkType", "deviceType", "time", "statistic"};
+    private String[][] dimensions = {
+            {"86744", "86544"},
+            {"Search", "Content", "Mixed"},
+            {"Smartphone"},
+            {"day&2017-12-31&2017-12-31", "day&2018-01-01&2018-01-01"},
+            {"clickCount", "cost"},
+    };
+    private String[] values = {
+            "0", "0",
+            "0", "0",
+            "0", "0",
+            "0", "0",
+            "0", "0",
+            "0", "0",
+            "0", "0",
+            "2", "0.49695420625",
+            "0", "0",
+            "0", "0",
+            "0", "0",
+            "0", "0"
+    };
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
