e0babad11867e347a2023de48096943144addb8b
Makar Kalancha <makarkalancha@gmail.com>
2018-10-08 23:51:56 -0400

    localdatetime: tests

--- src/com/everything/strings/RemoveVowel.java
+++ src/com/everything/strings/RemoveVowel.java
@@ -10,13 +10,20 @@ import org.apache.commons.lang3.StringUtils;
 public class RemoveVowel {
 
     public static void main(String[] args) {
-        System.out.println(removeVowel("BUSINESS_ACCOUNT_ID ("));
+        System.out.println(removeVowel("CATEGORY_ID,\n" +
+                "    TAX_ID,\n" +
+                "    BENEFICIARY_ID,\n" +
+                "    DESCRIPTION1,\n" +
+                "    D_SUB_TOTAL,\n" +
+                "    D_TOTAL,\n" +
+                "    C_SUB_TOTAL,\n" +
+                "    C_TOTAL ("));
     }
 
     public static String removeVowel(String stringToClean){
         //https://simple.wikipedia.org/wiki/Vowel
         //        char[] vowels = {'A', 'E', 'I', 'O', 'U', 'Y'};
-        final String vowels = "AEIOUY_, ";
+        final String vowels = "AEIOUY_, \n";
         stringToClean = stringToClean.toUpperCase();
         return StringUtils.replaceChars(stringToClean, vowels, "");
     }
